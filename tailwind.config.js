01900bcb43bdb2ccdbd6710eaf9d26106208c1b3
Katchie <41813266+Ka-Da-Sci@users.noreply.github.com>
2024-09-06 11:42:02 +0100

    Update the sections.js and sections-screenshots folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js}', // Specify the paths where Tailwind should look for class names
+    './src/**/*.{html,js}', // Add the paths where Tailwind can find your dynamic classes
+    './test.html',
+    './src/assets/index.js'
   ],
-  darkMode: 'media', // or 'class' or false for no dark mode
   theme: {
-    width: {
-      'custom-width': 'var(--beforewidth)',
-    },
-    extend: {
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
-  corePlugins: {
-    preflight: false,
-  }
 }
